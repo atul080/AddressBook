567e6d45c606a5274dfc88e8c48d12df9faa4894
Atul Srivastav <atul.srivastav.ap@nielsen.com>
2022-01-03 21:27:55 +0530

    Added: Complete address book functionality added.

--- AddressBook/src/com/addressbook/AddressBook.java
+++ AddressBook/src/com/addressbook/AddressBook.java
@@ -9,26 +9,7 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//All AllContactss class
-class AllContacts {
-    //instance variable declaration
-    public String firstName, lastName, address, city, state, email,phoneNum;
-    public  int zip;
 
-
-    //constructor
-    AllContacts(String firstName, String lastName, String address, String city, String state, int zip, String phoneNum, String email)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNum = phoneNum;
-        this.email = email;
-    }
-}
 /*
 * Address Book class to perform all the functionalities of address book.
 * */
